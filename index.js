209710dc81df500c40b19961cff7a207b519754f
Rori Hinds <r_ori@hotmail.co.uk>
2019-04-17 18:46:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function logShout(string) {
   console.log(string.toUpperCase())
 }
 
-function logShout(string) {
-  console.log(string.toUpperCase())
+function logWhisper(string) {
+  console.log(string.toLowerCase())
 }
 

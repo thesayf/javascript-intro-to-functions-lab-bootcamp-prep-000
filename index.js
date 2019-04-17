1c65ebdeb70d3a5ae8e3bbc3ebd1336b0b8d5797
Rori Hinds <r_ori@hotmail.co.uk>
2019-04-17 18:28:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,10 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log(string.toLowerCase()) 
+  console.log(`Hello, ${firstName}!`)
 }
+ 
+sayHelloTo('Isabel') // "Hello, Isabel!"
+sayHelloTo("Jane") // "Hello, Jane!"
+sayHelloTo("R2-D2") // "Hello, R2-D2!"
+sayHelloTo(1) 

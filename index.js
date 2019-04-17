510e27c5b320100739b2e48c519e80ac1fdea68b
Rori Hinds <r_ori@hotmail.co.uk>
2019-04-17 18:50:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,18 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
+function sayHiToGrandma (aWord) {
+  var cantAnswer = "I can't hear you!";
+  var yesAnswer = "YES INDEED!";
+  var lovAnswer = "I love you, too.";
+  if (aWord.toLowerCase(aWord) === aWord) {
+    return cantAnswer;
+  }
+  else if (aWord.toUpperCase(aWord) === aWord) {
+    return yesAnswer;
+  }
+  else if ("I love you, Grandma." === aWord) {
+    return lovAnswer
+  }
+}
+
